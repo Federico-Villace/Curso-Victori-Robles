5f55e4e8c0b3d70a8605b6c4b70d92d9de53d49a
Federico Villace <federicovillace@gmail.com>
2020-09-15 20:06:49 -0300

    actualizacion2-15-09-2020

--- JS/ejercicio-72-1-Promesas-Fetch.js
+++ JS/ejercicio-72-1-Promesas-Fetch.js
@@ -2,8 +2,8 @@
 
 window.addEventListener('load', ()=>{
 
-var info = document.querySelector(".info"); 
-var UsuarioEspecifico = document.querySelector("#UsuarioEspecifico"); 
+    var info = document.querySelector(".info"); 
+    var UsuarioEspecifico = document.querySelector("#UsuarioEspecifico"); 
 
 // Promesas 
     get_Usuarios()
@@ -16,40 +16,38 @@ var UsuarioEspecifico = document.querySelector("#UsuarioEspecifico");
             .then(user => {
             mostrarUsuario(user.data);
             })
-   
 
 // get 
-function get_UsuarioEspecifico(){
-    return fetch("https://reqres.in/api/users/2");
-}
-
-function get_Usuarios(){
-    return fetch("https://reqres.in/api/users");
-}
-
+    function get_UsuarioEspecifico()
+    {
+        return fetch("https://reqres.in/api/users/2");
+    }
+    function get_Usuarios()
+    {
+        return fetch("https://reqres.in/api/users");
+    }
 
 // metodos
-function mostrarUsuario(user)
-{
-    let datos = document.createElement("h3");
-    let avatar = document.createElement("img");
-    datos.innerHTML = user.first_name + " " + user.last_name;
-    avatar.src = user.avatar;   
+    function mostrarUsuario(user)
+    {
+        let datos = document.createElement("h3");
+        let avatar = document.createElement("img");
+        datos.innerHTML = user.first_name + " " + user.last_name;
+        avatar.src = user.avatar;   
 
-    UsuarioEspecifico.appendChild(avatar);
-    UsuarioEspecifico.appendChild(datos);
+        UsuarioEspecifico.appendChild(avatar);
+        UsuarioEspecifico.appendChild(datos);
     
-    document.querySelector("#UsuarioEspecifico .loading").style.display = "none";    
-}
-
-function listadoUsuarios(usuarios)
-{
-    usuarios.map((user, i) => {
-        let datos = document.createElement("h3"); 
-        datos.innerHTML = i + " - " + user.first_name+ " " + user.last_name; 
-        info.appendChild(datos); 
+        document.querySelector("#UsuarioEspecifico .loading").style.display = "none";    
+    }
+    function listadoUsuarios(usuarios)
+    {
+        usuarios.map((user, i) => {
+            let datos = document.createElement("h3"); 
+            datos.innerHTML = i + " - " + user.first_name+ " " + user.last_name; 
+            info.appendChild(datos); 
     });
-    document.querySelector(".loading").style.display = "none";    
-}
- 
+        document.querySelector(".loading").style.display = "none";    
+    }
+
 });  
--- JS/ejercicio-72-2-Promesas-Fetch.js
+++ JS/ejercicio-72-2-Promesas-Fetch.js
@@ -3,8 +3,9 @@
 window.addEventListener('load', function() {
 
     var UsuarioEspecifico = document.querySelector('#UsuarioEspecifico'); 
+    var Usuarios = document.querySelector("#caja"); 
 
-
+// promesas 
     get_Usuario()
         .then(function (data) {
             return data.json(); 
@@ -13,18 +14,44 @@ window.addEventListener('load', function() {
             muestra_Usuario(user.data);
         })
 
-    function get_Usuario(){
+    get_Usuarios()
+        .then(function(data) {
+            return data.json(); 
+        })
+        .then(function(user){
+            muestra_Usuarios(user.data); 
+        })
+        
+// get         
+    function get_Usuario()
+    {
         return fetch("https://reqres.in/api/users/2"); 
     }
 
-    function muestra_Usuario(user){
+    function get_Usuarios()
+    {
+        return fetch("https://reqres.in/api/users?page=2"); 
+    }
 
-        var datos = document.createElement("h3");
+// metodos 
+    function muestra_Usuario(user)
+    {
+        let datos = document.createElement("h3");
         datos.innerHTML = user.first_name + " " + user.last_name; 
         UsuarioEspecifico.append(datos);
         document.querySelector("#UsuarioEspecifico .loading").style.display = "none"; 
-
     }
 
-    // MISMO EJERCICIO QUE EL 72-1 PERO ESTE ESTA REALIZDO CON LA SINTAXIS MAS LARGA DE LAS FUNCIONES DE CALLBACK 
+    function muestra_Usuarios(users)
+    {         
+        users.forEach(function(users)  
+        {
+            let datos = document.createElement("h3");
+            datos.innerHTML = "Nombre: " + users.first_name + " - Apellido: " + users.last_name + " - Mail:" + users.email; 
+            Usuarios.append(datos); 
+        });   
+        document.querySelector("#caja .loading").style.display = "none"; 
+    }
+
+// MISMO EJERCICIO QUE EL 72-1 PERO ESTE ESTA REALIZDO CON LA SINTAXIS MAS LARGA DE LAS FUNCIONES DE CALLBACK 
 }); 
